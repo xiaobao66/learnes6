52e50697d3dfb59343e573b9b7f06a3ebd1bddec
xiaobao66 <xiaobao66@bingyan.net>
2016-12-02 17:57:38 +0800

    使用html-webpack-plugin插件自动生成html文件，并为js文件名添加hash值

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -13,7 +14,7 @@ module.exports = {
     },
     output: {
         path: __dirname + '/scripts/dist/',
-        filename: '[name].js'
+        filename: '[name]_[chunkhash:8].js'
     },
     watch: true,
     module: {
@@ -24,5 +25,42 @@ module.exports = {
             ],
             loader: 'babel-loader'
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'learn es6',
+            filename: '../../demos/index.html',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            title: 'let_and_const',
+            filename: '../../demos/let_and_const.html',
+            chunks: ['let_and_const']
+        }),
+        new HtmlWebpackPlugin({
+            title: '解构赋值',
+            filename: '../../demos/destructuring_assign.html',
+            chunks: ['destructuring_assign']
+        }),
+        new HtmlWebpackPlugin({
+            title: '字符串扩展',
+            filename: '../../demos/string_extend.html',
+            chunks: ['string_extend']
+        }),
+        new HtmlWebpackPlugin({
+            title: '数组的扩展',
+            filename: '../../demos/array_extend.html',
+            chunks: ['array_extend']
+        }),
+        new HtmlWebpackPlugin({
+            title: '函数的扩展',
+            filename: '../../demos/function_extend.html',
+            chunks: ['function_extend']
+        }),
+        new HtmlWebpackPlugin({
+            title: '对象的扩展',
+            filename: '../../demos/object_extend.html',
+            chunks: ['object_extend']
+        })
+    ]
 };

45c743fcc17c9b3f86df5bb5ca62b03e6cdb8b12
xiaobao66 <xiaobao66@bingyan.net>
2016-12-08 16:06:17 +0800

    增加string_extend注释

--- scripts/dist/string_extend-11098b1f.js
+++ scripts/dist/string_extend-11098b1f.js
@@ -52,13 +52,15 @@
 	/*
 	 * 字符串的Unicode表示法
 	 * */
-	var s1 = 'A';
-	console.log(s1);
+	var s1 = '\uD842\uDFB7';
+	var ss1 = '\uD842\uDFB7';
+	console.log(s1, ss1); // s1 = 𠮷, s2 = 𠮷
 
 	//获取字符码点
 	var s2 = '𠮷A';
-	console.log(s1.codePointAt(0));
-	console.log(s2.codePointAt(0));
+	console.log(s2.codePointAt(0)); // 134071
+	console.log(s2.codePointAt(1)); // 57271
+	console.log(s2.codePointAt(2)); // 97
 
 	var _iteratorNormalCompletion = true;
 	var _didIteratorError = false;
@@ -70,6 +72,8 @@
 
 	        console.log(ch.codePointAt(0).toString(16));
 	    }
+	    // 20bb7
+	    // 41
 
 	    //检测一个字符是两字节还四字节
 	} catch (err) {
@@ -102,29 +106,33 @@
 	var ch1 = String.fromCodePoint(0x20BB7),
 	    ch2 = String.fromCodePoint(0x41);
 
-	console.log(ch1, ch2);
+	console.log(ch1, ch2); // ch1 = 𠮷, ch2 = A
 
 	//includes,startsWith,endsWith
 	var s3 = 'hello world';
 
-	console.log(s3.includes('hello'));
-	console.log(s3.startsWith('world', 6));
-	console.log(s3.endsWith('hello', 5));
+	console.log(s3.includes('hello')); // true
+	console.log(s3.startsWith('world', 6)); // true
+	console.log(s3.endsWith('hello', 5)); // true
 
 	//repeat
-	console.log('ha'.repeat(3));
+	console.log('ha'.repeat(3)); // hahaha
 
 	//模板字符串
 	var str4 = 'hello world';
 	var str5 = 'hello\nworld';
 
-	console.log(str4);
+	console.log(str4); // hello world
 	console.log(str5);
+	/*
+	   hello
+	   world
+	*/
 
 	var name = 'xiaobao',
 	    str6 = 'hello ' + name;
 
-	console.log(str6);
+	console.log(str6); // hello xiaobao
 
 	var html = '<ul>\n<li>a</li>\n<li>b</li>\n<li>c</li>\n</ul>';
 
--- scripts/src/string_extend.js
+++ scripts/src/string_extend.js
@@ -4,17 +4,21 @@
 /*
  * 字符串的Unicode表示法
  * */
-let s1 = '\u{41}';
-console.log(s1);
+let s1 = '\u{20BB7}';
+let ss1 = '\uD842\uDFB7';
+console.log(s1, ss1); // s1 = 𠮷, s2 = 𠮷
 
 //获取字符码点
 let s2 = '𠮷A';
-console.log(s1.codePointAt(0));
-console.log(s2.codePointAt(0));
+console.log(s2.codePointAt(0)); // 134071
+console.log(s2.codePointAt(1)); // 57271
+console.log(s2.codePointAt(2)); // 97
 
 for (let ch of s2) {
     console.log(ch.codePointAt(0).toString(16));
 }
+// 20bb7
+// 41
 
 //检测一个字符是两字节还四字节
 function is32Bit(ch) {
@@ -32,30 +36,34 @@ console.log(is32Bit('A')); //false
 let ch1 = String.fromCodePoint(0x20BB7),
     ch2 = String.fromCodePoint(0x41);
 
-console.log(ch1, ch2);
+console.log(ch1, ch2); // ch1 = 𠮷, ch2 = A
 
 //includes,startsWith,endsWith
 var s3 = 'hello world';
 
-console.log(s3.includes('hello'));
-console.log(s3.startsWith('world', 6));
-console.log(s3.endsWith('hello', 5));
+console.log(s3.includes('hello')); // true
+console.log(s3.startsWith('world', 6)); // true
+console.log(s3.endsWith('hello', 5)); // true
 
 //repeat
-console.log('ha'.repeat(3));
+console.log('ha'.repeat(3)); // hahaha
 
 //模板字符串
 let str4 = `hello world`;
 let str5 = `hello
 world`;
 
-console.log(str4);
+console.log(str4); // hello world
 console.log(str5);
+/*
+   hello
+   world
+*/
 
 let name = 'xiaobao',
     str6 = `hello ${name}`;
 
-console.log(str6);
+console.log(str6); // hello xiaobao
 
 let html = `<ul>
 <li>a</li>
